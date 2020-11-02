8ff3ef3267c36be74ff2d0d358eb1949299f6911
Aman Thanvi <22306104+amanthanvi@users.noreply.github.com>
2020-11-02 04:04:49 -0500

    Hello World project completion and upload to repo.

---
+++ C++/Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,13 @@
+// Hello World.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
